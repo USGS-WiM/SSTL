2f5ca841ae3da0a93fe7e5629d7e15f73935b82f
09Kathy <kdooley@usgs.gov>
2021-09-14 17:29:12 -0500

    start over

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -13,7 +13,7 @@
         <l-tile-layer :url="url" :attribution="attribution" />
         <l-marker :lat-lng="withPopup">
           <l-popup>
-            <div @click="innerClick">
+            <div @click="showLongText">
               I am a popup
               <p v-show="showParagraph">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
@@ -56,6 +56,8 @@ export default {
   },
   data() {
     return {
+      totalVuePackages: null,
+      myList: [],
       zoom: 13,
       center: latLng(47.41322, -1.219482),
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -75,16 +77,52 @@ export default {
   methods: {
     zoomUpdate(zoom) {
       this.currentZoom = zoom;
+      this.created();
     },
     centerUpdate(center) {
       this.currentCenter = center;
+      this.created();
     },
     showLongText() {
       this.showParagraph = !this.showParagraph;
+      this.created();
     },
     innerClick() {
       alert("Click!");
+      this.created();
     },
-  },
+created() {
+  
+  console.log("made it to created");
+    // Simple GET request using fetch
+
+    var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+var client = new HttpClient();
+client.get('https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php', function(response) {
+    console.log("response", response);
+});
+
+   // fetch("https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php")
+   //   .then(data => (this.totalVuePackages = data), console.log("this.totalVuePackages", this.totalVuePackages))
+    //  .then(data => (this.totalVuePackages = data.total), console.log("this.totalVuePackages2", this.totalVuePackages));
+      
+  
+  }
+}
+    /* getJson(){
+      $.getJSON('https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php', function(myTask){ console.log("myTask", myTask)
+        this.myList = myTask}.bind(this))} 
+  },*/
 };
 </script>
