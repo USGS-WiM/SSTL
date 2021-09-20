f36e4cd65e0a8e749c24ef55aaa0868203942180
09Kathy <kdooley@usgs.gov>
2021-09-20 09:15:13 -0500

    start over

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -11,9 +11,20 @@
         @update:zoom="zoomUpdate"
       >
         <l-tile-layer :url="url" :attribution="attribution" />
+        <!-- markers (these ones use custom wim divIcon styling not leaflet default) -->
+        <l-layer-group layer-type="overlay" name="Markers" :visible="true">
+          <l-marker
+            v-for="marker in markers"
+            :key="marker.id"
+            :visible="marker.visible"
+            :lat-lng="marker.position"
+            :icon="nwisIcon"
+          />
+          <l-popup>"marker.id"</l-popup>
+        </l-layer-group>
         <l-marker :lat-lng="withPopup">
           <l-popup>
-            <div @click="showLongText">
+            <div @click="innerClick">
               I am a popup
               <p v-show="showParagraph">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
@@ -41,10 +52,16 @@
 </template>
 
 <script>
-import { latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+import { latLng, Icon, divIcon } from "leaflet";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LPopup,
+  LTooltip,
+  LLayerGroup,
+} from "vue2-leaflet";
 import "leaflet/dist/leaflet.css";
-
 export default {
   name: "Example",
   components: {
@@ -53,11 +70,10 @@ export default {
     LMarker,
     LPopup,
     LTooltip,
+    LLayerGroup,
   },
   data() {
     return {
-      totalVuePackages: null,
-      myList: [],
       zoom: 13,
       center: latLng(47.41322, -1.219482),
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -68,61 +84,112 @@ export default {
       currentZoom: 11.5,
       currentCenter: latLng(47.41322, -1.219482),
       showParagraph: false,
+      markers: [],
+      nwisIcon: divIcon({
+        className: "wmm-circle wmm-mutedblue  wmm-size-20",
+      }), //custom WIM icons
       mapOptions: {
         zoomSnap: 0.5,
       },
       showMap: true,
     };
   },
+  //if you want to load a geojson layer
+  async created() {
+    // const response = await fetch(
+    //   "https://stn.wim.usgs.gov/STNServices/SensorViews.geojson?ViewType=baro_view&"
+    //);
+    // this.geojson = await response.json();
+    // console.log("this.geojson", this.geojson);
+
+    let markerArray = [];
+
+    const response2 = await fetch(
+      "https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php"
+    );
+    let cameraJSON = await response2.json();
+
+    console.log("cameraJSON", cameraJSON);
+    for (let i = 0; i < cameraJSON.data.length; i++) {
+      // console.log("cameraJSON.data[i]", cameraJSON.data[i]);
+      let site = cameraJSON.data[i];
+      if (site.nwis_values) {
+        let cameraID = site._id;
+        let tempSiteArr = {
+          id: null,
+          position: null,
+          draggable: true,
+          visible: true,
+        };
+        tempSiteArr.position = {
+          lat: site.nwis_values.lat,
+          lng: site.nwis_values.lng,
+        };
+        tempSiteArr.id = cameraID;
+        markerArray.push(tempSiteArr);
+        //console.log("markerArray", markerArray);
+      }
+    }
+    console.log("markerArray", markerArray);
+    this.markers = markerArray;
+
+    console.log("this.markers", this.markers);
+
+    /* let HttpClient = function() {
+      this.get = function(aUrl, aCallback) {
+        let anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() {
+          if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+            aCallback(anHttpRequest.responseText);
+        };
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
+      };
+    };
+    let client = new HttpClient();
+    client.get(
+      "https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php",
+      function(response) {
+        let cameraJSON = JSON.parse(response);
+        for (let i = 0; i < cameraJSON.length; i++) {
+          let site = cameraJSON.data[i];
+          if (site.nwis_values) {
+            let cameraID = site.id;
+            console.log("cameraID", cameraID);
+            this.testThing = 3;
+          }
+        }
+      }
+    ); */
+  },
   methods: {
     zoomUpdate(zoom) {
       this.currentZoom = zoom;
-      this.created();
     },
     centerUpdate(center) {
       this.currentCenter = center;
-      this.created();
     },
     showLongText() {
       this.showParagraph = !this.showParagraph;
-      this.created();
     },
     innerClick() {
       alert("Click!");
-      this.created();
     },
-created() {
-  
-  console.log("made it to created");
-    // Simple GET request using fetch
-
-    var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        }
-
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
-    }
-}
-var client = new HttpClient();
-client.get('https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php', function(response) {
-    console.log("response", response);
-});
-
-   // fetch("https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php")
-   //   .then(data => (this.totalVuePackages = data), console.log("this.totalVuePackages", this.totalVuePackages))
-    //  .then(data => (this.totalVuePackages = data.total), console.log("this.totalVuePackages2", this.totalVuePackages));
-      
-  
-  }
-}
-    /* getJson(){
-      $.getJSON('https://apps.usgs.gov/sstl/php/getAllEnabledCameras.php', function(myTask){ console.log("myTask", myTask)
-        this.myList = myTask}.bind(this))} 
-  },*/
+  },
 };
 </script>
+
+<style scoped>
+/*placing "scoped" after style affects only the current file*/
+@import "../styles/markers.css";
+button {
+  background-color: #eceef3;
+  border-radius: 0;
+  border: none;
+  box-shadow: 0 3px 6px rgba(30, 39, 50, 0.2), 0 3px 6px rgba(30, 39, 50, 0.2);
+  color: #6f758e;
+  font-size: 9pt;
+  letter-spacing: 1px;
+  padding: 5px;
+}
+</style>
