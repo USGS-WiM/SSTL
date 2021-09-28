bef439c70da6aa917b5025278ab5912128fc4ab0
09Kathy <kdooley@usgs.gov>
2021-09-28 08:34:10 -0500

    clean-up

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    drawerState: true
-  },
-  mutations: {
-    toggleDrawerState (state, data) {
-      state.drawerState = data
-    }
-  },
-  getters : {
-    drawerState: (state) => state.drawerState
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
